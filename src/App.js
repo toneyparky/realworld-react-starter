1272eda9301d49d3211723aefdd96799e355ca00
toneyparky <qkrtmddhks95@gmail.com>
2020-06-02 17:10:12 +0900

    feat: set route for rest pages
    - set href on Header.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import {Route} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home"
+import Auth from "./pages/Auth";
+import Article from "./pages/Article";
+import Settings from "./pages/Settings";
+import Editor from "./pages/Editor";
 
 const App = () => {
   return (
     <>
       <Header/>
       <Route path="/" component={Home} exact/>
+      <Route path="/article" component={Article} exact/>
+      <Route path="/editor" component={Editor} exact/>
+      <Route path="/settings" component={Settings} exact/>
+      <Route path="/auth" component={Auth} exact/>
       <Footer/>
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,23 @@ import React from "react";
 const Header = () => (
   <nav className="navbar navbar-light">
     <div className="container">
-      <a className="navbar-brand" href="index.html">conduit</a>
+      <a className="navbar-brand" href="/">conduit</a>
       <ul className="nav navbar-nav pull-xs-right">
         <li className="nav-item">
-          <a className="nav-link active" href="">Home</a>
+          <a className="nav-link active" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="">
+          <a className="nav-link" href="/editor">
             <i className="ion-compose"></i>&nbsp;New Post
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="">
+          <a className="nav-link" href="/settings">
             <i className="ion-gear-a"></i>&nbsp;Settings
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="">Sign up</a>
+          <a className="nav-link" href="/auth">Sign up</a>
         </li>
       </ul>
     </div>
